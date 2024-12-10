352125026379bd5ed8c206e7d40f594b5d777137
Sayan Chatterjee <sayanc354@gmail.com>
2024-12-10 07:41:21 +0530

    jghv

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
+from fastapi import FastAPI
 
 templates = Jinja2Templates(directory="templates")
-
+app = FastAPI()
 @app.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
